3845796f607cbf9bd15ac11c982516242a9eb273
Haseeb Akbar <haseeb2000@hotmail.com>
2018-01-22 16:28:28 +0000

    Able to findAllData, findById, findById and update, FindbyidDelete

--- src/FireBase/index.js
+++ src/FireBase/index.js
@@ -5,20 +5,50 @@ const config = require('./config');
 firebase.initializeApp(config);
 
 const db = firebase.database();
-// console.log(db);
 
 const ref = db.ref('news');
-ref.orderByChild("last").limitToLast(2).on("child_added", function(snapshot) {
-  console.log(snapshot.key);
-});
 
-// const newsfeed = {
-//   name : 'will',
-//   last :'baker'
-// };
+// find all database data
+function newsFieldData() {
+  ref.on("value", function (newsFeed) {
+    console.log(newsFeed.val())
+  })
+}
 
-// news.push(newsfeed)
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(console.log);
+// Able to gather all data by firstname
+function gatherData(firstName) {
+  firebase.database().ref('news').orderByChild('name').equalTo(firstName).on('value', (res) => {
+    console.log(res.val())
+  });
+}
+
+// Able to gather last name data
+function gatherDataLast(lastName) {
+  firebase.database().ref('news').orderByChild('last').equalTo(lastName).on('value', (res) => {
+    console.log(res.val())
+  });
+}
+
+//Able to find by ID
+function getById(id) {
+  firebase.database().ref(`news/${id}`).once('value', function (res) {
+    let exists = res.val();
+    if (exists) console.log(exists);
+  });
+}
+
+// Able to update userData
+function writeUserData(id, Firstname, lastName) {
+  firebase.database().ref(`news/${id}`).set({
+    last: lastName,
+    name: Firstname
+  });
+}
+
+// Able to delete user data
+function deleteUserData(id) {
+  firebase.database().ref(`news/${id}`).remove()
+  console.log("It has been deleted")
+}
+
+
