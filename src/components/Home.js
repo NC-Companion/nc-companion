58492344c8ab7141f477a1c3df08a7aba5f7ccf5
Will Barker <32540837+barks1212@users.noreply.github.com>
2018-01-31 20:10:00 +0000

    link to set ghub display name (#56)

--- src/components/Home.js
+++ src/components/Home.js
@@ -59,7 +59,7 @@ class Home extends React.Component {
               </section>
               <section className="defaultPadding">
                 <h2 className="title homePageGreeting">
-                  <span className="has-text-danger">Hello</span> {this.props.authUser.displayName ? this.props.authUser.displayName.split(' ')[0] : 'Northcoder'}.
+                  <span className="has-text-danger">Hello</span> {this.props.authUser.displayName ? this.props.authUser.displayName.split(' ')[0] :<span>Northcoder. <a href="https://github.com/settings/profile" className=" has-text-weight-light is-italic is-size-7">Set your display name on Github.</a></span>}
                 </h2>
                 <h3 className="subtitle is-size-4">You are on Week 3.</h3>
               </section>
